68610310d100e28307ff23d50a894a90419e3735
Eric Zhang <ezhang2@stuy.edu>
2017-05-31 00:10:02 -0400

    Finished pathfinding

--- Driver/Pathfind.java
+++ Driver/Pathfind.java
@@ -1,29 +1,76 @@
-/*public class Pathfind {
+import java.util.*;
 
-  //double{known?, distance, prevNode}
-  HashMap<City, double[]> distances;
-  City start, current;
-
-  public Pathfind(int range, City startCity) {
-    start = startCity;
-    current = startCity;
-    distances = new HashMap(Driver.getCities().size());
-    for (City city : Driver.getCities()) {
-      if (city.equals(startCity)) {
-        distances.put(city, {
-          1, 0, -1
-        }
-        );
-      } else {
-        distances.put(city, {
-          0, 9999, -1
-        }
-        );
-      }
+public class Pathfind {
+    
+    private static double getDistance(City begin, City end) {
+	return Math.sqrt(Math.pow(begin.getXcor() - end.getXcor(),2) + Math.pow(begin.getYcor() - end.getYcor(),2));
     }
-  }
 
-  public HashMap<City, City[]> getPaths() {
-    for (City city : Driver.getCities()) {
-      if (!(city.equals(start)) {
-        */
+
+    public static HashMap<City, ArrayList<City>> getPaths(int range, City start, ArrayList<City> cities) {
+
+	//initialize values
+	//double{visited, distance};
+	HashMap<City, double[]> distances = new HashMap(cities.size());
+	double[] placeholder;
+	for (City city : cities) {
+	    if (city.equals(start)) {
+		distances.put(city, new double[] {1, 0});
+	    } else {
+		distances.put(city, new double[] {0, Integer.MAX_VALUE});
+	    }
+	}
+
+	City current = start;
+
+	HashMap<City, ArrayList<City>> retHash = new HashMap<City, ArrayList<City>>();
+	ArrayList<City> tempPath;
+	double dist;
+	double[] tempInsert;
+	double minDist = 0;
+
+	while (minDist < Integer.MAX_VALUE) {
+
+	    //set current node to visited
+	    tempInsert = distances.get(current);
+	    tempInsert[0] = 1;
+	    distances.put(current, tempInsert);
+
+	    //find tentative distances
+	    for (City city : cities) {
+		if (!(city.equals(current)) && distances.get(city)[0] == 0) {
+		    dist = getDistance(city, current);
+		    //replace distance with shorter one if applicable
+		    if (dist < distances.get(city)[1]) {
+			tempInsert = distances.get(city);
+			tempInsert[1] = dist;
+			distances.put(city, tempInsert);
+			//replace path
+			if (!(current.equals(start))) {
+			    tempPath = new ArrayList<City>(retHash.get(current));
+			}
+			else {
+			    tempPath = new ArrayList<City>();
+			}
+			tempPath.add(current);
+			retHash.put(city, tempPath);
+		    }
+		}
+	    }
+
+
+	    //find next destination
+	    minDist = Integer.MAX_VALUE;
+	    for (City city: cities) {
+		if (distances.get(city)[0] == 0) {
+		    if (distances.get(city)[1] < minDist) {
+			current = city;
+			minDist = distances.get(city)[1];
+		    }
+		}
+	    }
+	}
+
+	return retHash;
+    }
+}
